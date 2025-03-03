efb31ac9c745b4e6f7eac2ebcc65291e1bbd1443
gszczure <grzegorz.sz1202@outlook.com>
2025-03-03 16:29:57 +0100

    Upgrade appearance and functionality for notification

--- src/main/resources/build/notification.js
+++ src/main/resources/build/notification.js
@@ -1,34 +1,41 @@
-let notificationTimeout;
+let notificationTimeout
 
 function initializeNotification() {
-    const notification = document.getElementById('notification');
-    const notificationMessage = document.getElementById('notification-message');
-    const notificationClose = document.getElementById('notification-close');
+    const notification = document.getElementById("notification")
+    const notificationMessage = document.getElementById("notification-message")
+    const notificationClose = document.getElementById("notification-close")
 
     if (!notification || !notificationMessage || !notificationClose) {
-        console.error('Notification elements not found. Make sure they are present in the DOM.');
-        return;
+        console.error("Notification elements not found. Make sure they are present in the DOM.")
+        return
     }
 
-    window.showNotification = function(message, duration = 3000) {
-        clearTimeout(notificationTimeout);
+    window.showNotification = (message, duration = 3000, type = "success") => {
+        clearTimeout(notificationTimeout)
 
-        notificationMessage.textContent = message;
-        notification.classList.add('show');
+        notificationMessage.textContent = message
+
+        notification.classList.remove("alert")
+
+        if (type === "alert") {
+            notification.classList.add("alert")
+        }
+
+        notification.classList.add("show")
 
         notificationTimeout = setTimeout(() => {
-            notification.classList.remove('show');
-        }, duration);
-    };
+            notification.classList.remove("show")
+        }, duration)
+    }
 
-    notificationClose.addEventListener('click', () => {
-        notification.classList.remove('show');
-    });
+    window.showAlert = (message, duration = 3000) => {
+        window.showNotification(message, duration, "alert")
+    }
+
+    notificationClose.addEventListener("click", () => {
+        notification.classList.remove("show")
+    })
 }
 
-// Wywołaj inicjalizację po załadowaniu DOM
-if (document.readyState === 'loading') {
-    document.addEventListener('DOMContentLoaded', initializeNotification);
-} else {
-    initializeNotification();
-}
+initializeNotification();
+
