0886c80db58289c7ad8327bc755dd4b60b6f5b78
gszczure <grzegorz.sz1202@outlook.com>
2025-03-04 14:25:00 +0100

    Testing timeout 10000 for save&create

--- src/main/resources/build/create-meeting.js
+++ src/main/resources/build/create-meeting.js
@@ -345,7 +345,7 @@ class Calendar {
                 setTimeout(() => {
                     //TODO przeslac code dalej zeby odrazu przenioslo do date-chose
                     window.location.href = "index.html";
-                }, 3000);
+                }, 10000);
             } else {
                 showAlert("Failed to create meeting.");
                 return null;
