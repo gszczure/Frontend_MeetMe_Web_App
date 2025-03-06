e6cd4275ec535e810a5df4e36046696d29f8b3b5
gszczure <grzegorz.sz1202@outlook.com>
2025-03-06 18:41:58 +0100

    Fix window.location.href

--- src/main/resources/build/index.js
+++ src/main/resources/build/index.js
@@ -65,8 +65,8 @@ function addMeetingToUI(meeting) {
     }
 
     meetingCard.addEventListener('click', () => {
-        // window.location.href = `https://backendmeetingapp-1.onrender.com/api/meetings/join/${meeting.code}`;
-        window.location.href = "date-chose.html";
+        window.location.href = `https://backendmeetingapp-1.onrender.com/api/meetings/join/${meeting.code}`;
+        // window.location.href = "date-chose.html";
     });
 
     meetingContainer.appendChild(meetingCard);
